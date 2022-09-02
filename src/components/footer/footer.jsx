9bb6e430e5c38ae99ce52b3c48a9dc89fc283f97
Cho SangHyun <98shcho@naver.com>
2022-09-02 20:39:24 +0900

    add: background img and complete footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import styles from "./footer.module.css";
+import { FaGithub, FaYoutube, FaFacebook, FaInstagramSquare } from "react-icons/fa";
 
 const Footer = (props) => {
     return(
-        <footer className={styles.footer}>footer</footer>
+        <footer className={styles.footer}>
+            <section className={styles.footer_content}>
+                <section className={styles.developer_info}>
+                    <p><b>개발자</b>  조상현 | <b>이메일</b> XXXXXXX@naver.com</p>
+                    <p><b>레포지토리</b> https://github.com/Cho-SangHyun/DanMeet</p>
+                    <p><b>전화</b> 010-4XX3-7XXX | <b>사업자 번호</b> XXX-XX-XXXXX</p>
+                </section>
+                <section className={styles.links}>
+                    <FaGithub />
+                    <FaYoutube />
+                    <FaFacebook />
+                    <FaInstagramSquare />
+                </section>
+            </section>
+        </footer>
     )
 }
 
