07393a4dc0f41517a18265dca5e8c5e562fcfd59
Cho SangHyun <98shcho@naver.com>
2022-09-07 17:28:49 +0900

    design: add signUpPage design

--- src/components/signUpPage/signupPage.jsx
+++ src/components/signUpPage/signupPage.jsx
@@ -1,6 +1,9 @@
 import React, {useState} from 'react';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { FaHome } from "react-icons/fa";
+import logo from "../../images/logo.png";
+import styles from './signUpPage.module.css';
 
 const SignUpPage = ({authService, database}) => {
     const navigate = useNavigate();
@@ -55,15 +58,19 @@ const SignUpPage = ({authService, database}) => {
     });
 
     return(
-        <div>회원가입
-            <form onSubmit={handleSubmit}>
-                <input type="email" name="email" placeholder='이메일' onChange={handleChange}/>
-                <input type="password" name="password1" placeholder='비밀번호' onChange={handleChange}/>
-                <input type="password" name="password2" placeholder='비밀번호 확인' onChange={handleChange}/>
-                <input type="text" name="nickname" placeholder='닉네임' onChange={handleChange}/>
-                <input type="number" name="classOf" placeholder='학번' onChange={handleChange}/>
-                <input type="text" name="major" placeholder='학과' onChange={handleChange}/>
-                <button>회원가입</button>
+        <div className={styles.signUpPage}>
+            <form className={styles.signUpForm} onSubmit={handleSubmit}>
+                <span className={styles.homeButton} onClick={() => {navigate("/")}}><FaHome /></span>
+                <img className={styles.logo} src={logo} alt="로고" />
+                <h2 className={styles.title}>회원가입</h2>
+                <p className={styles.description}>단밋에 들어와 친해지고 싶은 사람들을 만나보세요!</p>
+                <input className={styles.signUpInput} type="email" name="email" placeholder='이메일' onChange={handleChange}/>
+                <input className={styles.signUpInput} type="password" name="password1" placeholder='비밀번호' onChange={handleChange}/>
+                <input className={styles.signUpInput} type="password" name="password2" placeholder='비밀번호 확인' onChange={handleChange}/>
+                <input className={styles.signUpInput} type="text" name="nickname" placeholder='닉네임' onChange={handleChange}/>
+                <input className={styles.signUpInput} type="number" name="classOf" placeholder='학번' onChange={handleChange}/>
+                <input className={styles.signUpInput} type="text" name="major" placeholder='학과' onChange={handleChange}/>
+                <button className={styles.signUpButton}>회원가입</button>
             </form>
         </div>
     )
